a4b6934c733094a7ee39202cb2def256bcdbae54
Youssouph Djiba <youssouph.djiba@q2ebanking.com>
2018-11-05 17:41:45 -0600

    working on sending email

--- Dev/QuMedHome/src/offices/views.py
+++ Dev/QuMedHome/src/offices/views.py
@@ -1,12 +1,17 @@
 import random
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import TemplateView, DetailView, CreateView, ListView
+from django.core.mail import mail_admins, BadHeaderError, send_mail, EmailMessage
+
 # from django.views.generic.list import ListView
 
 from .forms import ProspectCreateForm, ProspectModelCreateForm
@@ -17,10 +22,13 @@ from .models import Prospect
 def prospect_create_view(request):
     form = ProspectModelCreateForm(request.POST or None)
     errors = None
+
     if form.is_valid():
         instance = form.save(commit=False)
         # instance.owner = request.user
         instance.save()
+        send_email(request)
+
         return HttpResponseRedirect("/confirmation/")
         # return HttpResponseRedirect("/prospects/")
 
@@ -35,7 +43,6 @@ def prospect_create_view(request):
         errors = form.errors
 
     template_name = 'offices/prospect_form.html'
-    template_name_modal = 'templates/confirmation_modal.html'
     context = {'form': form, 'errors': errors}
     return render(request, template_name, context)
 
@@ -71,3 +78,24 @@ class ProspectCreateView(CreateView):
     form_class = ProspectModelCreateForm
     template_name = 'offices/prospect_form.html'
     success_url = "/prospects/"
+
+
+def send_email(request):
+    subject = request.POST.get('company_name', '')
+    message = request.POST.get('message', 'New Business Inquiry')
+    from_email = request.POST.get('business_email', '')
+    to_email = ['ydjiba89@gmail.com']
+
+    if subject and message and from_email:
+        try:
+            email = EmailMessage(subject=subject, body=message, to=to_email)
+            email.send()
+
+            msg_sent = "have email has been sent"
+            print ('msg sent', msg_sent)
+            messages.success(request, msg_sent)
+        except BadHeaderError:
+            return HttpResponse('Invalid Header found.')
+        # return HttpResponseRedirect('/confirmation/')
+    else:
+        return HttpResponse('Make sure all fields are entered and valid')
