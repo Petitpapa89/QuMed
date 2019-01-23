f57c5a841b107bcb1d846b7b2717c23b76d4a259
Youssouph Djiba <youssouph.djiba@q2ebanking.com>
2019-01-22 21:54:05 -0600

    dockerized module

--- Dev/QuMedHome/src/QuMedHome/settings/base.py
+++ Dev/QuMedHome/src/QuMedHome/settings/base.py
@@ -126,6 +126,6 @@ STATICFILES_DIRS = [
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ydjiba89@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'icmtzhjqnutfzkly'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
